d439a46b4bdfb6dd2b0052d607e14b079a0f17a0
Jasmine Foong <jasfoong19@gmail.com>
2024-05-29 14:47:21 -0700

    contestants and challenges seed data; public images for both

---
+++ seed-data/challenges-data.js
@@ -0,0 +1,42 @@
+[
+    {
+        id: 1,
+        name: "Quest 0: Manual Teadmill Running",
+        season: 2,
+        team: false,
+        photo: "/images/challenges/s2-q0-manual-treadmill-running.jpg",
+        description: "The first round consists of 10 minutes where each contestant's running distance was logged. By the end of the round, the 50 with the least amount of distance covered were eliminated. The following round was shorter at seven minutes with only 10 of the 50 contestants making it to the final round. For the final round, the remaining 10 ran for five minutes. The final results will determine the winner who gets an advantage in the first official quest."
+    },
+    {
+        id: 2,
+        name: "Quest 1: One-on-one Death Match",
+        season: 2,
+        team: false,
+        photo: "/images/challenges/s2-q1-one-on-one-death-match.jpeg",
+        description: "The top 50 contestants from the Manual Treadmill Running challenge chooses an opponent from the bottom 50. They also got to choose their ideal quest terrain — playground, water or UFC cage. The objective of this quest is to battle their opponent to secure a medicine ball before the three minutes are up. All winners make it to the next quest."
+    },
+    {
+        id: 3,
+        name: "Quest 2: Five-on-Five Team Maze",
+        season: 2,
+        team: true,
+        photo: "/images/challenges/s2-q2-five-on-five-team-maze.jpeg",
+        description: "10 captains were selected from the remaining 50 contestants after the One-on-one Death Matches in Quest 2. The captains then formed their teams of five players based on needed strength, agility, and speed. Ten teams went up against each other in a race through a complex maze to add weighted bags to three different scales throughout the course. The weighted bags vary from 6-20kg. The team with the most secured weighted scales wins. The scales remained fixed until the five-minute mark when they tipped towards the team that had accumulated the most weight. There is one 15-minute round in this quest."
+    },
+    {
+        id: 4,
+        name: "Quest 2.5: Redemption Round",
+        season: 2,
+        team: false,
+        photo: "/images/challenges/s2-q2.5-redemption-round.jpeg",
+        description: "Eliminated teams get a chance to stay in the competition by wrestling each other to secure a pole before time runs out. The previously eliminated teams gathered in a pit surrounded by 10 poles. With the timer set to three minutes, the contestants must try and secure one of the poles by hugging it. If more than one person is at a pole, the person hugging the symbol is declared the winner. The quest had four rounds in total, with the number of poles and remaining contestants decreasing with each round. The winner of Quest 2.5 allowed to pick four contestants from the eliminated pool to return and become a new team for the next quest. This was the quest in which the 'Avengers' team was born."
+    },
+    {
+        id: 5,
+        name: "Quest 3: Mine Transportation",
+        season: 2,
+        team: true,
+        photo: "/images/challenges/s2-q3-mine-transportation.jpg",
+        description: "There are four rounds in the Mine Transportation Match. For each round, the team that places first gets three points, second gets two, and third gets one. At the end of Quest 3, the team with the least amount of points gets eliminated. Round 1 is a two-person minecraft haul. The first person secures as much weight as possible at the end of a track before pushing the minecraft back to the starting point and unloading. The second person will go back and add more weight to total two tons. The team to finish first wins. Round 2 is the exact same, except it involves only one team member. Round 3 is a two-person mid-air transit. A weighted bag suspended on monkey bars must be pushed to the endpoint. Once the contestant reaches the end, they must hurry back to their starting point through the same monkey bars. Round 4 is the same, with only one team member."
+    },
+]
---
+++ seed-data/contestants-data.js
@@ -0,0 +1,98 @@
+[
+    {
+        id: 1,
+        name: "Kim Dong Hyun (Cicada)",
+        season: 2,
+        career: "Pro MMA Fighter (Retired)",
+        photo: "/images/contestants/s2-kim-dong-hyun.png",
+        description: "Former #6 ranking in world UFC's welterweight division. Married to longtime girlfriend and have three children. Popular for his fun, goofy personality on variety shows."
+    },
+    {
+        id: 2,
+        name: "Lee Won Hee (Ippon)",
+        season: 2,
+        career: "National Judo Athlete (Retired)",
+        photo: "/images/contestants/s2-lee-won-hee.png",
+        description: "1x Olympic Gold Medalist, 1x World Champion Gold Medalist, 2x Asian Championship Gold Medalist. Considered one of the best Korean Judokas. Earned nickname for his 48 winning streak won by grand slams/ippons. He is married to a table tennis athlete and have two children together."
+    },
+    {
+        id: 3,
+        name: "Jung Ji Hyun",
+        season: 2,
+        career: "National Wrestler (Retired)",
+        photo: "/images/contestants/s2-jung-ji-hyun.png",
+        description: "1x Olympic Gold Medalist. Fishing enthusiast. Currently wrestling coach."
+    },
+    {
+        id: 4,
+        name: "Chong Te Se",
+        season: 2,
+        career: "National Soccer Player (Retired)",
+        photo: "/images/contestants/s2-chong-te-se.png",
+        description: "Currently owner of two fitness centers. Born in Japan to South Korean father and North Korean Mother. He represented the North Korean team due to Japan's anti-Zainichi Korean laws. Lives with wife and children in Japan."
+    },
+    {
+        id: 5,
+        name: "Andre Jin",
+        season: 2,
+        career: "National Rugby Player",
+        photo: "/images/contestants/s2-andre-jin.png",
+        description: "Father is American and mother is Korean. He has been featured on variety shows. His mother is famous supermodel, Kim Dong Su."
+    },
+    {
+        id: 6,
+        name: "Kim Jee Hyuk",
+        season: 2,
+        career: "National Rowing Athlete",
+        photo: "/images/contestants/s2-kim-jee-hyuk.png",
+        description: "Kim Jee-hyuk is a Korean national rowing team athlete who has won multiple national medals. He was eliminated in Season 2 in Round 2 and was eliminated again in Round 4 after he was chosen to join the Avengers team. He's often reunited with his former castmates and got married to his girlfriend."
+    },
+    {
+        id: 7,
+        name: "Kim Min Su (Thanos)",
+        season: 2,
+        career: "IFBB Pro Bodybuilder, Olympian (Retired)",
+        photo: "/images/contestants/s2-kim-min-su.png",
+        description: "Trained to be potential rugby player in his teens, but later found passion in weightlifting and bodybuilding. He was chosen by Jung Ji-hyun to be in the Avengers team but was later eliminated in Round 4. Kim was a favorite due to his impressive physicality and strength. He was brought back during the redemption challenge. But he was elimianted during the barrel relay."
+    },
+    {
+        id: 8,
+        name: "Amotti (Kim Jae Hong)",
+        season: 2,
+        career: "Crossfitter",
+        photo: "/images/contestants/s2-amotti.png",
+        description: "He's popular among the cast for his fitness YouTube channel, as well as his cardio and strength abilities. Amotti is the season 2 champion."
+    },
+    {
+        id: 9,
+        name: "Emmanuel",
+        season: 2,
+        career: "IFBB Pro Bodybuilder, Fitness Model",
+        photo: "/images/contestants/s2-emmanuel.png",
+        description: "Emmanuel is born in Nigeria and is currently working out of South Korea. He is fluent in Korean. He was eliminated in his one-on-one match against Kim Dong-hyun. "
+    },
+    {
+        id: 10,
+        name: "Jung Dae Jin",
+        season: 2,
+        career: "Sports Model, Bodybuilder",
+        photo: "/images/contestants/s2-jung-dae-jin.png",
+        description: "3x Pro NABBA Korea Sports Model Competition. He's also nicknamed Superbody Jin for his impressive physique. He was a part of Kim's team and later elimianted after the relay challenge."
+    },
+    {
+        id: 11,
+        name: "Hong Beom Seok",
+        season: 2,
+        career: "Special Forces (Retired), Firefighter",
+        photo: "/images/contestants/s2-hong-beom-seok.png",
+        description: "Enlisted in the Special Warfare Command in 2006 and eventually ended special forces career as a sergeant in 2017. Was appointed Firefighter as a Rescue Specialist and won 2018 World Firefighter Competition beating 150 firefighters in a four-day event. Also participated in the intense special forces competition show 'The Soldiers.' Married with two sons."
+    },
+    {
+        id: 12,
+        name: "Gibson",
+        season: 2,
+        career: "US Army Major",
+        photo: "/images/contestants/s2-gibson.png",
+        description: "Lives in the states with wife and two children. Gibson is a United States Army Officer from the 411th Contracting Support Brigade at Camp Humphreys and is a powerlifter. Gibson made it to Round 4. "
+    },
+]
