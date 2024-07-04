72a9b8088a5b0efc30945c0e45fd34c514d9eb93
Jasmine Foong <jasfoong19@gmail.com>
2024-07-04 15:33:29 -0700

    add season 1 challenges in seed data

--- seed-data/challenges-data.js
+++ seed-data/challenges-data.js
@@ -1,6 +1,118 @@
 module.exports = [
     {
         id: 1,
+        name: "Hanging Challenge",
+        season: 1,
+        team: false,
+        photo: "images/challenges/s1-q0-hanging.jpeg",
+        description: "The 100 contestants were split into two groups of 50 (based on a chosen number) and competed to hang from a bar raised above water for as long as they could. The contestants were ranked according to how long they hung onto the bar before falling into the water."
+    },
+    {
+        id: 2,
+        name: "One-on-one Death Match",
+        season: 1,
+        team: false,
+        photo: "images/challenges/s1-q1-one-on-one-death-match.png",
+        description: "Contestants compete one-on-one to gain possession of a ball. The contestant with possession of the ball at the 3-minute mark proceeded to the next Quest, while the other contestant was eliminated. Before the quest, contestants were ranked based on their Quest 0 timing, and contestants that were ranked higher had the benefit to choose their opponent and the type of Battle Arena they want to compete in: Battle Arena A included many obstacles, emphasizing contestant agility and speed. Battle Arena B was simpler, with a large flat area of sand with a pool of water in the middle, emphasizing contestant strength and grappling ability. If neither contestant had possession after three minutes, the competition was restarted with one minute on the clock. Fifty contestants were eliminated in the process."
+    },
+    {
+        id: 3,
+        name: "Moving Sand",
+        season: 1,
+        team: true,
+        photo: "images/challenges/s1-q2-moving-sand.jpg",
+        description: "Before Quest 2, the remaining 50 contestants were required to choose three contestants with whom they wanted to team up. The ten contestants with the most votes were declared team leaders and were ranked based on the numbers of votes they received. Contestants lined up for the team leader they wanted to join. In order of their number of votes, each team leader picked four members to form a team of five. Contestants who were not chosen had to pick another team. For Quest 2, each team of five had to work together to build a bridge, and then carry bags of sand across the bridge. Teams were not allowed to pass bags of sand to another team member; they had to carry the bags of sand individually to the other end. After twelve minutes, the team with the least amount of sand accumulated was eliminated."
+    },
+    {
+        id: 4,
+        name: "Revival",
+        season: 1,
+        team: false,
+        photo: "images/challenges/s1-q2.5-revival.png",
+        description: "Contestants who were eliminated in Quest 2 were taken to another room, where they found their torsos hanging from the ceiling. The game required all contestants to hold onto a rope connected to their torso, with 40% of the contestant's total body weight added, for as long as possible to prevent it from falling to the ground and breaking apart. The last five contestants holding their rope re-entered the competition for Quest 3."
+    },
+    {
+        id: 5,
+        name: "Ship Moving",
+        season: 1,
+        team: true,
+        photo: "images/challenges/s1-q3-ship-moving.jpg",
+        description: "In Quest 3, the six teams had to join forces to create three teams of ten. The teams spent some time debating how they would join, with the last two excluded teams being joined by default. The combined teams of ten had to each work together to dig up oak barrels from the sand and load them onto a 1.5-tonne ship. The additional weight of the oak barrels was 0.5-tonne, meaning the combined load weighed 2.0 tonnes. The teams then had to drag/push the ship off a wooden platform onto logs in the sand, across the sand-filled arena on the logs, then up a wooden ramp until they could pull a mooring rope over an iron stake. The two teams with the fastest time progressed to the next Quest, while the team with the slowest time was eliminated. Team 1 and Team 2 won with times of 13:34 and 19:55 respectively. Team 3 lost with a time of 22:15 and was eliminated."
+    },
+    {
+        id: 6,
+        name: "Punishment of Atlas",
+        season: 1,
+        team: true,
+        photo: "images/challenges/s1-q4.1-punishment-of-atlas.jpg",
+        description: "The semi-finals were divided into five different games based on ancient mythology. The winner of each game would advance to the final, while the remaining contestants would be eliminated. The Punishment of Atlas had contestants hold a 50 kg (110.2 lb) rock for as long as possible, until only one was left standing. Shin Bo-mi-rae was unable to lift the stone above her shoulders at all. Kim Kang-min dropped out after about 15 minutes. At just past the two hour mark, Kim Sik also dropped out, leaving Jo Jin-hyeong as the winner. Jo later commented that he was near his own breaking point."
+    },
+    {
+        id: 7,
+        name: "Fire of Prometheus",
+        season: 1,
+        team: true,
+        photo: "images/challenges/s1-q4.2-fire-of-prometheus.png",
+        description: "The Fire of Prometheus had contestants running through an obstacle course in repetition to grab a torch; the person holding the last torch would win. Nippert stumbled on the first obstacle in the first rep, and never regained the pace of the other three. The remaining matches were closer, hinging on Park Jin-yong scaling the obstacle cleanly and landing in a position to immediately begin running, while Miracle's body was twisting as he came over, leaving him just a moment slower to start running towards the torches."
+    },
+    {
+        id: 8,
+        name: "Wings of Icarus",
+        season: 1,
+        team: true,
+        photo: "images/challenges/s1-q4.3-wings-of-icarus.jpg",
+        description: "The Wings of Icarus had contestants climb a rope and endure climbing for as long as possible, until only one contestant remained. Song A-reum and Kim Da-young, the last two women in the competition, dropped out first. Son Hee-dong lasted a little bit longer, but ice climber Kim Min-cheol was easily able to secure victory."
+    },
+    {
+        id: 9,
+        name: "Tail of Ouroboros",
+        season: 1,
+        team: true,
+        photo: "images/challenges/s1-q4.4-tail-of-ouroboros.jpg",
+        description: "The Tail of Ouroboros had contestants chase each other around a track in a bid to tag out the person ahead of them, until only one remained."
+    },
+    {
+        id: 10,
+        name: "Punishment of Sisyphus",
+        season: 1,
+        team: true,
+        photo: "images/challenges/s1-q4.5-punishment-of-sisyphus.png",
+        description: "The Punishment of Sisyphus had contestants push a 100kg boulder up and over a hill every 40 seconds, as many times as possible, until only one remained. All four were able to push the boulder over the hill many times, with the winner having the greatest stamina and cardio endurance."
+    },
+    {
+        id: 11,
+        name: "Tug of War",
+        season: 1,
+        team: false,
+        photo: "images/challenges/s1-q5.1-tug-of-war.png",
+        description: "The first game was a five-way tug of war, with all contestants trying to secure a key in front of them in order to unlock their padlock. Kim Min-cheol and Woo Jin-yong were the last two to unlock their padlocks, and did so nearly simultaneously."
+    },
+    {
+        id: 12,
+        name: "Square Flip",
+        season: 1,
+        team: false,
+        photo: "images/challenges/s1-q5.2-square-flip.jpg",
+        description: "The remaining four contestants split into teams of two and tried to flip as many tiles as possible for five minutes. The playing field contained 66 tiles, with 33 of each colour facing up to begin the match. One side of each tile was white and the other was black, and the team with most tiles flipped to their colour won. The losing team had to compete with each other one on one for three minutes. Jung Hae-min, as the first competitor to release himself in the tug-of-war game, was allowed to select his partner. After first selecting Woo Jin-yong, he changed his mind and instead selected Park Jin-yong. This team won the first team round by a score of 37–29. This left Jo Jin-hyeong and Woo Jin-yong to go head-to-head to progress to the third game. The smaller and quicker Woo easily won the head-to-head round 48-18, meaning Jo Jin-hyeong was eliminated."
+    },
+    {
+        id: 13,
+        name: "Triangular Shuttle Run",
+        season: 1,
+        team: false,
+        photo: "images/challenges/s1-q5.3-triangular-shuttle-run.jpg",
+        description: "The third game was a triangular shuttle run, with the remaining three contestants forced to run back and forth between their start point and the 'next apex' before the next start signal. As the game progresses, the time to complete the shuttle runs gets shorter. The contestant who could not make it back to their original point before the next signal lost the game. After 85 reps, done without cease, Park Jin-yong collapsed at his starting position, which led to his elimination."
+    },
+    {
+        id: 14,
+        name: "Infinite Rope Pull",
+        season: 1,
+        team: false,
+        photo: "images/challenges/s1-q5.4-infinite-rope-pull.jpg",
+        description: "The final game was infinite rope-pulling. The two contestants had to pull their rope until it was loose, grab the other end of the rope, and then destroy their opponent's torso. Woo Jin-yong won the final game and thus won Physical: 100, while Jung Hae-min was eliminated."
+    },
+    {
+        id: 15,
         name: "Manual Treadmill Running",
         season: 2,
         team: false,
@@ -8,7 +120,7 @@ module.exports = [
         description: "The initiation quest of season 2. The first round consists of 10 minutes where each contestant's running distance was logged. By the end of the round, the 50 with the least amount of distance covered were eliminated. The following round was shorter at seven minutes with only 10 of the 50 contestants making it to the final round. For the final round, the remaining 10 ran for five minutes. The final results will determine the winner who gets an advantage in the first official quest."
     },
     {
-        id: 2,
+        id: 16,
         name: "One-on-one Death Match",
         season: 2,
         team: false,
@@ -16,7 +128,7 @@ module.exports = [
         description: "The top 50 contestants from the Manual Treadmill Running challenge chooses an opponent from the bottom 50. They also got to choose their ideal quest terrain — playground, water or UFC cage. The objective of this quest is to battle their opponent to secure a medicine ball before the three minutes are up. All winners make it to the next quest."
     },
     {
-        id: 3,
+        id: 17,
         name: "Five-on-Five Team Maze",
         season: 2,
         team: true,
@@ -24,7 +136,7 @@ module.exports = [
         description: "For quest 2 of season 2, 10 captains were selected from the remaining 50 contestants after the One-on-one Death Matches in Quest 2. The captains then formed their teams of five players based on needed strength, agility, and speed. Ten teams went up against each other in a race through a complex maze to add weighted bags to three different scales throughout the course. The weighted bags vary from 6-20kg. The team with the most secured weighted scales wins. The scales remained fixed until the five-minute mark when they tipped towards the team that had accumulated the most weight. There is one 15-minute round in this quest."
     },
     {
-        id: 4,
+        id: 18,
         name: "Redemption Round",
         season: 2,
         team: false,
@@ -32,7 +144,7 @@ module.exports = [
         description: "Quest 2.5: Eliminated teams get a chance to stay in the competition by wrestling each other to secure a pole before time runs out. The previously eliminated teams gathered in a pit surrounded by 10 poles. With the timer set to three minutes, the contestants must try and secure one of the poles by hugging it. If more than one person is at a pole, the person hugging the symbol is declared the winner. The quest had four rounds in total, with the number of poles and remaining contestants decreasing with each round. The winner of Quest 2.5 allowed to pick four contestants from the eliminated pool to return and become a new team for the next quest. This was the quest in which the 'Avengers' team was born."
     },
     {
-        id: 5,
+        id: 19,
         name: "Mine Transportation",
         season: 2,
         team: true,
@@ -40,7 +152,7 @@ module.exports = [
         description: "Quest 3: There are four rounds in the Mine Transportation Match. For each round, the team that places first gets three points, second gets two, and third gets one. At the end of Quest 3, the team with the least amount of points gets eliminated. Round 1 is a two-person minecraft haul. The first person secures as much weight as possible at the end of a track before pushing the minecraft back to the starting point and unloading. The second person will go back and add more weight to total two tons. The team to finish first wins. Round 2 is the exact same, except it involves only one team member. Round 3 is a two-person mid-air transit. A weighted bag suspended on monkey bars must be pushed to the endpoint. Once the contestant reaches the end, they must hurry back to their starting point through the same monkey bars. Round 4 is the same, with only one team member."
     },
     {
-        id: 6,
+        id: 20,
         name: "Intrateam Survival",
         season: 2,
         team: false,
@@ -48,7 +160,7 @@ module.exports = [
         description: "Quest 4. The remaining teams were thrown a curveball when the voice announced the teammates would be going against each other. Only one of each team will make it to the final quest. The challenge is simple but requires endurance and strength. The first-come roller race requires each team member to haul a roller across a field with humps and a water ditch. Each roller weighs 150kg (330lbs) and has straps for the contestants to secure to themselves to pull it. Each contestant must haul the roller across the field, run back to the starting point, and haul a second roller. They must try to outbeat each other in speed and time and run back to the starting point as quickly as possible. The person unable to complete the challenge or come in last is eliminated. Each team would go through four rounds until only one person remains."
     },
     {
-        id: 7,
+        id: 21,
         name: "Torso Hold",
         season: 2,
         team: false,
@@ -56,7 +168,7 @@ module.exports = [
         description: "Round 1 of the final quest. Similar to the redemption challenge in Season 1, each contest holds a rope connected to a pulley and their plaster torso plus additional weight adding up to 40% of their body weight. The first person to drop their plaster torso is eliminated.Justin Harvey was eliminated."
     },
     {
-        id: 8,
+        id: 22,
         name: "Infinite Squats",
         season: 2,
         team: false,
@@ -64,7 +176,7 @@ module.exports = [
         description: "Round 2 of the final quest was tied into the theme of the series. The final three contestants had to perform 30 weighted squats in each round until one calls it quits. But there was an added challenge. A conveyor belt would add real coal after each round to add more weight, making each set of 30 squats more difficult than the last. Andrew Jin was eliminated."
     },
     {
-        id: 9,
+        id: 23,
         name: "Pole Push",
         season: 2,
         team: false,
