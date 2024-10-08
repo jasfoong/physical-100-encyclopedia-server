f5aa56f504b341f4596d54d601f6cdda7707b95b
Jasmine Foong <jasfoong19@gmail.com>
2024-10-08 10:46:23 -0700

    configure migration and seed directories in knexfile

--- knexfile.js
+++ knexfile.js
@@ -11,5 +11,11 @@ module.exports = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     charset: "utf8"
+  },
+  migrations: {
+    directory: './migrations',
+  },
+  seeds: {
+    directory: './seeds',
   }
 };
